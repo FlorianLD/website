204689ede8f91609d36de10537e4bbda241637c1
Florian Le Du <florian.ledu@gmail.com>
2024-08-24 11:30:31 +0200

    Project description content update

--- scripts.js
+++ scripts.js
@@ -1,34 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
     
-    const elements = document.querySelectorAll('.case');
-    const imageStuff = document.querySelector('.case-image');
+    const cases = document.querySelectorAll('.case');
     const project_desc = document.querySelector('.project-desc');
 
-    elements[0].addEventListener('mouseout', function () {
+    cases[0].addEventListener('mouseout', function () {
         project_desc.firstChild.nodeValue = 
-        "While invocies are key elements in most business activities, leveraging their data consistently requires a way to extract in a structured output. ";
+        "While invocies are key elements in most business activities, retrieving their data consistently requires an easy way to extract it in a structured output. This POC uses computer vision to extract the invoice information and send it to a Google Sheet for further analysis.";
     });
     
-    elements[1].addEventListener('mouseover', function () {
-        /*imageStuff.src = 'icons/price_monitoring_tool.png';*/
-        project_desc.firstChild.nodeValue = 'ABC';
-        console.log(project_desc)
+    cases[1].addEventListener('mouseover', function () {
+        project_desc.firstChild.nodeValue = 
+        "Several frameworks of the python ecosystem are targeted towards data apps development. These frameworks offer an alternative way to develop BI solutions without relying on traditional vendors like Microsoft or Salesforce. This demo app uses the Dash Plotly framework.";
     });
 
-    elements[2].addEventListener('mouseout', function () {
-        /*imageStuff.src = 'icons/image.png';*/
-        project_desc.firstChild.nodeValue = 'GHI';
+    cases[2].addEventListener('mouseout', function () {
+        project_desc.firstChild.nodeValue = 
+        "Product categorization is an important process in domains such as digital commerce. A precise categorization contributes to an optimal customer experience and better conversion rate. This POC leverages AI capabilities to streamline and automate the categorization process.";
     });
-
-    console.log(imageStuff)
 });
-
-document.addEventListener('DOMcontentLoaded', function () {
-
-    const project_desc = document.querySelector('.project-desc');
-    const projects = document.querySelectorAll('.case');
-
-    projects[0].addEventListener('click', function () {
-        project_desc.child = 'ABC';
-    })
-})
